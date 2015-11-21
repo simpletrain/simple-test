ed83dc42841c52d2defaec4a1992a6ad0ced64aa
ming <zsy498761554@live.com>
2015-11-21 17:30:54 +0800

    pass spec-1 & spec-2

--- src/calculate-second.js
+++ src/calculate-second.js
@@ -1,3 +1,19 @@
 function calculate_median(arr) {
+    var result = [];
+    for(var i =0;i < arr.length; i++){
+        if(i%2 === 1){
+            result.push(arr[i]);
+        }
+    }
+
+    result.sort(function(a, b) {
+        return a - b;
+    });
+
+    if(result.length%2 === 1){
+        return result[(result.length-1)/2];
+    } else {
+        return (result[result.length/2]+result[result.length/2-1])/2;
+    }
 
 }
